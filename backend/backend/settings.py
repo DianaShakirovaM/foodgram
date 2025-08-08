aa9ec8262b12fdc380513204da1fc0004a4e0973
Diana Shakirova <diana.shak1rova@yandex.ru>
2025-08-08 21:04:37 +0500

    correct mistakes 6

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-
+SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET_KEY')
 ALLOWED_HOSTS = os.getenv(
     'ALLOWED_HOSTS',
     '127.0.0.1,localhost,host.docker.internal'
