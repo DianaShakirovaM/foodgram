60da2cf86101317294a22acbaf05d165be183c76
Diana Shakirova <diana.shak1rova@yandex.ru>
2025-08-09 13:34:56 +0500

    correct mistakes 7

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
 from django.utils.safestring import mark_safe
-from django.contrib.auth.admin import UserAdmin
 
 from .filters import CookingTimeFilter
 from .models import (
